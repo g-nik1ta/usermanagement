d9106fa5564acc24a79283932c11f83e3954acfa
Nikita Gonohov <gonohovn@gmail.com>
2022-11-21 02:07:33 +0200

    delete unnecessary files

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,6 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-    publicPath: process.env.NODE_ENV === 'production'
-        ? '/usermanagement/'
-        : '/',
+    publicPath: process.env.NODE_ENV === 'production' ? '/usermanagement/' : '/',
     transpileDependencies: [
         'vuetify'
     ]
