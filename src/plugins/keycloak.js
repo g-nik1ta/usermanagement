8bef2f8bdfe7a83829fffa61cb4d8de5f1fb30ba
Nikita Gonohov <gonohovn@gmail.com>
2022-11-28 17:04:33 +0200

    change initoptions on keycloak nks

--- src/plugins/keycloak.js
+++ src/plugins/keycloak.js
@@ -1,17 +1,17 @@
 import Vue from 'vue'
 import Keycloak from 'keycloak-js'
 
-const initOptions = {
-    url: 'http://localhost:8180',
-    realm: 'usermanagement-realm',
-    clientId: 'usermanagement-client'
-}
-
 // const initOptions = {
-//     url: 'https://keycloak.nks.com.ua',
-//     realm: 'rm_dev_realm',
-//     clientId: 'ui_dev'
+//     url: 'http://localhost:8180',
+//     realm: 'usermanagement-realm',
+//     clientId: 'usermanagement-client'
 // }
+
+const initOptions = {
+    url: 'https://keycloak.nks.com.ua/auth/',
+    realm: 'rm_dev_realm',
+    clientId: 'ui_dev'
+}
 
 const keycloak = Keycloak(initOptions)
 
